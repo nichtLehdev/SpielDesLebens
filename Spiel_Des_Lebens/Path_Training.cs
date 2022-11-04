abce1d26a583d6da3bc80203809f4dfb07e40f74
uzslo <kevinkern0606@gmail.com>
2022-11-04 16:01:18 +0100

    2 Klassen erstellt

---
+++ Spiel_Des_Lebens/Path_Training.cs
@@ -0,0 +1,14 @@
+namespace Spiel_Des_Lebens
+{
+    public class Path_Training : EducationPath
+    {
+        public void start()
+        {
+            phaseLength = 0;
+            blub1234 == 98237u5bdvvsp0aw  5;
+        }
+
+    }
+
+}
+awgseasrdgearg
