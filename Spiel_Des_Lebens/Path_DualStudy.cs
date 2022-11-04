c13ad66daf8229064da00d25c172abd1d0cc29f2
uzslo <kevinkern0606@gmail.com>
2022-11-04 17:46:28 +0100

    Grundstruktur des Spiels angelegt.

--- Spiel_Des_Lebens/Path_DualStudy.cs
+++ Spiel_Des_Lebens/Path_DualStudy.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Spiel_Des_Lebens
 {
-    internal class Path_DualStudy
+    internal class Path_DualStudy : EducationPath
     {
+
+        public override void main(Direction direction)
+        {
+            phaseLength = 3;
+            base.main(direction);
+        }
+
+        public override string getProfession()
+        {
+            switch(direction)
+            {
+                case Direction.Social:
+                    return "Angewandte Gesundheits- und Pflegewissenschaften";
+                case Direction.Business:
+                    return "BWL";
+                case Direction.Science:
+                    return "Angewandte Physik";
+                case Direction.Stem:
+                    return "Angewandtes Informatikstudium";
+                case Direction.Civil:
+                    return "Steuerwesen";
+            }
+            return "ERROR, getProfession failed!";
+        }
     }
 }
--- Spiel_Des_Lebens/Path_Study.cs
+++ Spiel_Des_Lebens/Path_Study.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Spiel_Des_Lebens
 {
-    internal class Path_Study
+    internal class Path_Study : EducationPath
     {
+        public override void main(Direction direction)
+        {
+            phaseLength = 3;
+            base.main(direction);
+        }
+
+        public override string getProfession()
+        {
+            switch (direction)
+            {
+                case Direction.Social:
+                    return "Medizinstudium";
+                case Direction.Business:
+                    return "BWL";
+                case Direction.Science:
+                    return "Physikstudium";
+                case Direction.Stem:
+                    return "Informatikstudium";
+                case Direction.Civil:
+                    return "Jurastudium";
+            }
+            return "ERROR, getProfession failed!";
+        }
     }
 }
