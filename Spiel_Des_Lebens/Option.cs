4283f96beedd56eb134084dd6f709dad08e1fe89
Lars Lehmann <lars@Larss-MacBook-Pro.local>
2022-11-08 16:52:41 +0100

    Added text value in Option class

--- Spiel_Des_Lebens/Option.cs
+++ Spiel_Des_Lebens/Option.cs
@@ -9,11 +9,13 @@ namespace Spiel_Des_Lebens
     class Option
     {
         private string title;
+        private string text;
         private Stats optionStat;
 
-        public Option(string title, Stats optionStat)
+        public Option(string title, string text, Stats optionStat)
         {
             this.title = title;
+            this.text = text;
             this.optionStat = optionStat;
         }
     }
